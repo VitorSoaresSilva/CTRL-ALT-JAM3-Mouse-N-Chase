947f4faa3404ffed6b483354e51e50975640b5d6
Dudu <eduardoromeueberts@gmail.com>
2024-05-18 13:19:38 -0300

    Implementado missões aleatórias no botão Duty

--- Assets/_Developers/Romeu/Scripts/UIButton.cs
+++ Assets/_Developers/Romeu/Scripts/UIButton.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -10,7 +12,15 @@ public class UIButton : MonoBehaviour, ISelectHandler, ISubmitHandler
     [field: SerializeField] public Vector3 PointerPosition { get; private set; }
     [field: SerializeField] public Vector3 PointerRotation { get; private set; }
     [SerializeField] private float transitionDuration = 1f;
-    public MissionType missionType;
+    [SerializeField] private bool randomMission = false;
+    [SerializeField] private MissionType _missionType;
+    public MissionType missionType
+    {
+        // Get random mission based on the missionType enum
+        get => (randomMission) ? (MissionType)UnityEngine.Random.Range(0, (int)Enum.GetValues(typeof(MissionType)).Cast<MissionType>().Max()) : _missionType;
+        
+        set => _missionType = value;
+    }
 
     public Button Button { get; private set; }
 
