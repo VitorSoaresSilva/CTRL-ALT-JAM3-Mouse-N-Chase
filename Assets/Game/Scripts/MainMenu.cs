02c6ec718c8d98ae282056b54b2d522f5f845cf5
RetrKill0 <retrkill0@gmail.com>
2024-05-19 06:43:30 -0300

    Comentado código que deu problema na build

--- Assets/Game/Scripts/MainMenu.cs
+++ Assets/Game/Scripts/MainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor.SearchService;
+//using UnityEditor.SearchService;
 using UnityEngine.EventSystems;
 
 //By FJB and Romeu
