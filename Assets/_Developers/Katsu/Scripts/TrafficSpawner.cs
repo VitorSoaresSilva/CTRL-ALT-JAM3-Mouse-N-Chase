e2aba058e52870e25beb1bf690f1d84d5e5ddd87
Vitor Soares Silva <vitrola.vitor@gmail.com>
2024-05-19 21:45:26 -0300

    Traffic car

--- Assets/_Developers/Katsu/Scripts/TrafficSpawner.cs
+++ Assets/_Developers/Katsu/Scripts/TrafficSpawner.cs
@@ -7,35 +7,36 @@ namespace _Developers.Vitor
 {
     public class TrafficSpawner : MonoBehaviour
     {
-        public CarFollowPath[] trafficCarPrefabs;
-        public float[] spawnPoints;
+        public TrafficCarFollowPath[] trafficCarPrefabs;
+        // public float[] spawnPoints;
         public float minXOffset = -5f;
         public float maxXOffset = 5f;
         private PathGenerator _pathGenerator;
+        public int amountSpawn = 20;
 
         private void Awake()
         {
             _pathGenerator = GetComponent<PathGenerator>();
+            
         }
 
         private void Start()
         {
-            for (int i = 0; i < spawnPoints.Length; i++)
+            for (int i = 0; i < amountSpawn; i++)
             {
-                SpawnTrafficCar(spawnPoints[i]);
+                SpawnTrafficCar(amountSpawn);
             }
         }
 
         private void SpawnTrafficCar(float spawnDistance)
         {
             Vector3 spawnPosition = _pathGenerator.pathCreatorInstance.path.GetPointAtDistance(spawnDistance);
-            CarFollowPath selectedTrafficCarPrefab = trafficCarPrefabs[Random.Range(0, trafficCarPrefabs.Length)];
-            CarFollowPath trafficCarInstance = Instantiate(selectedTrafficCarPrefab, spawnPosition, Quaternion.identity);
-            trafficCarInstance.pathCreator = _pathGenerator.pathCreatorInstance;
+            TrafficCarFollowPath selectedTrafficCarPrefab = trafficCarPrefabs[Random.Range(0, trafficCarPrefabs.Length)];
+            TrafficCarFollowPath trafficCarInstance = Instantiate(selectedTrafficCarPrefab, spawnPosition, Quaternion.identity);
+            trafficCarInstance.Init(_pathGenerator.pathCreatorInstance,
+                Random.Range(0f, _pathGenerator.pathCreatorInstance.path.length),
+                Random.Range(0, 2) < 0.5f ? -1 : 1);
 
-            // Define aleatoriamente o deslocamento x do carro de tráfego
-            float xOffset = Random.Range(minXOffset, maxXOffset);
-            trafficCarInstance.transform.position += new Vector3(xOffset, 0, 0);
         }
     }
 }
---
+++ Assets/_Developers/Vitor/TrafficCarFollowPath.cs
@@ -0,0 +1,75 @@
+using PathCreation;
+using UnityEngine;
+
+namespace _Developers.Vitor
+{
+    public class TrafficCarFollowPath : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float speed = 5;
+        public float lateralSpeed = 5;
+        public float yOffset = 0;
+        public float distanceTravelled;
+        public Transform car;
+        
+        // Variáveis para controle de movimentação aleatória
+        private float horizontalInput = 0f;
+        private float changeDirectionInterval = 8f; // Intervalo de tempo para mudar a direção
+        private float lastDirectionChangeTime;
+        public float lateralLimit = 7f;
+        private bool isBoosted = false;
+        private float boostEndTime = 0f;
+        private float _direction = 1;
+        public bool changeDirection = false;
+        public float minXOffset = -5f;
+        public float maxXOffset = 5f;
+        void Start() {
+            if (pathCreator != null)
+            {
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+        }
+
+        public void Init(PathCreator pathCreatorRef, float initialDistanceTravelled, float direction)
+        {
+            distanceTravelled = initialDistanceTravelled;
+            pathCreator = pathCreatorRef;
+            _direction = direction;
+            float xOffset = Random.Range(minXOffset, maxXOffset);
+            car.transform.localPosition += new Vector3(xOffset, 0, 0);
+        }
+        
+        void FixedUpdate()
+        {
+            if (changeDirection)
+            {
+                if (Time.time - lastDirectionChangeTime > changeDirectionInterval)
+                {
+                    horizontalInput = Random.Range(-1f, 1f);
+                    lastDirectionChangeTime = Time.time;
+                }
+            }
+            if (horizontalInput != 0)
+            {
+                float deltaX = horizontalInput * lateralSpeed * Time.fixedDeltaTime;
+                car.transform.Translate(Vector3.right * deltaX);
+                
+                // Limita o movimento lateral
+                Vector3 carPosition = car.transform.localPosition;
+                carPosition.x = Mathf.Clamp(carPosition.x, -lateralLimit, lateralLimit);
+                car.transform.localPosition = carPosition;
+            }
+            if (pathCreator != null)
+            {
+                distanceTravelled += speed * _direction * Time.fixedDeltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction) + new Vector3(0, yOffset, 0);
+                transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+            }
+        }
+        
+        void OnPathChanged() {
+            // distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+    }
+}
