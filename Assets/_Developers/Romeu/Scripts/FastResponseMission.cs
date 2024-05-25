7b516ccc1815adecc74abf2ceb93943caaac3ad8
RetrKill0 <retrkill0@gmail.com>
2024-05-25 19:16:58 -0300

    detalhe missão

--- Assets/_Developers/Romeu/Scripts/FastResponseMission.cs
+++ Assets/_Developers/Romeu/Scripts/FastResponseMission.cs
@@ -8,6 +8,9 @@ public class FastResponseMission : MonoBehaviour
     public GameplayManager gameplayManager;
     public PlayerCar playerCar;
     public GameObject policeTunnelPrefab;
+    public AudioClip damageAudio;
+    private AudioSource audioSource;
+    [SerializeField] private PoliceTunnel policeTunnel;
 
     public int damageTaken = 0;
     public bool tunnelSpawned = false;
@@ -31,12 +34,20 @@ public class FastResponseMission : MonoBehaviour
             gameplayManager = FindObjectOfType<GameplayManager>();
         }
         if(playerCar == null) playerCar = FindObjectOfType<PlayerCar>();
-        
+
+        audioSource = GetComponent<AudioSource>();
+
+        policeTunnel = FindObjectOfType<PoliceTunnel>();
+
         playerCar.carDamage.onDamage = () =>
         {
             damageTaken++;
-            Debug.Log("Damage taken");
-            if(damageTaken > 2)
+            //Debug.Log("Damage taken");
+            if(damageTaken == 4)
+            {
+                audioSource.PlayOneShot(damageAudio);
+            }
+            if(damageTaken > 5)
             {
                 gameplayManager.EndGameplay(false);
             }
@@ -47,16 +58,18 @@ public class FastResponseMission : MonoBehaviour
     {
         if(gameplayManager.currentLap >= lapsToWin && !completed)
         {
-            Debug.Log("Mission Complete");
+            //Debug.Log("Mission Complete");
             gameplayManager.EndGameplay(true);
             completed = true;
         }
 
-        //if(gameplayManager.currentLap >= lapsToWin - 1 && !tunnelSpawned)
-        //{
-        //    PathGenerator.instance.tunnelPrefab = policeTunnelPrefab;
-        //    Debug.Log("Changed tunnel prefab");
-        //    tunnelSpawned = true;
-        //}
+        if (gameplayManager.currentLap >= lapsToWin - 1 && !tunnelSpawned)
+        {
+            policeTunnel.SetActive(true);
+            //PathGenerator.instance.tunnelPrefab = policeTunnelPrefab;
+            //Debug.Log("Changed tunnel prefab");
+            Debug.Log("Tunnel spawned");
+            tunnelSpawned = true;
+        }
     }
 }
